cf27d91e2a162001642a4ccebed348ea00fb521e
falin08 <falianaalifia08@gmail.com>
2024-11-24 20:43:46 +0700

    corsMiddleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,17 +15,19 @@ class CorsMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $response = $next($request);
+        // Tangani preflight request OPTIONS
+        if ($request->getMethod() === 'OPTIONS') {
+            return response()->json([], 200, [
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+            ]);
+        }
 
+        // Lanjutkan permintaan dan tambahkan header CORS pada response
+        $response = $next($request);
         $response->headers->set('Access-Control-Allow-Origin', '*'); // Allow all origins
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-        // Tangani preflight request OPTIONS
-        if ($request->getMethod() === 'OPTIONS') {
-            return response()->json([], 200);
-        }
-
-        return $response;
     }
 }
