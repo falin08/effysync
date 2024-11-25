654a5b4f0cdc423ed8a8d15bd6cb87b52137577f
falin08 <falianaalifia08@gmail.com>
2024-11-25 09:46:10 +0700

    hapus ensure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \App\Http\Middleware\CorsMiddleware::class, // Menambahkan CORS Middleware
         ]);
         $middleware->alias([
